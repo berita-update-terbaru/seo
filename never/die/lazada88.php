59cfbb32de80c62eff68ebbf52192db2b7157023
TSB <tsbteam88@gmail.com>
2025-09-04 01:17:53 +0700

    Update lazada88.php

--- never/die/lazada88.php
+++ never/die/lazada88.php
@@ -33,8 +33,8 @@ function Ram()
 $judul = str_replace("%20", "-", Ram());
 $b = Ram();
 $nb = Ram();
-$title = $judul . ": PTSP MTsN 2 ENREKANG";
-$desc = $judul . " Pelayanan Cepat, Tepat, dan Profesional untuk Anda.";
+$title = $judul . ": SPMB SMAN 17 KAB.TANGERANG";
+$desc = $judul . " INFO DAFTAR ULANG KLIK DISINI";
 $kw = $judul . ", daftar " . $judul . ", maxwin " . $judul . ", link alternatif " . $judul . ", situs " . $judul . ", slot " . $judul . ",togel " . $judul . ",agen " . $judul . ",slot gacor, slot88";
 $banner = "https://i.ibb.co/9mCMn0Vp/photo-2025-08-17-23-17-19.jpg";
 $favicon = "https://i.ibb.co/G4Ny32Fx/image.png";
@@ -42,7 +42,7 @@ $logo = "https://i.ibb.co/Cpg3GVvz/gacoflash.gif";
 $button = "https://i.ibb.co/P75hKL2/daftar-bossku.gif";
 
 // URL with hyphens instead of %20
-$url = 'https://ptsp.mtsn-2enrekang.sch.id/' . str_replace(" ", "-", $judul);
+$url = 'https://sman17kabtangerang.sch.id/' . str_replace(" ", "-", $judul);
 $dftr = "https://t.ly/akun88";
 $amp = 'https://pantang-mundur.pages.dev/' . str_replace(" ", "-", $judul);
 
